b70d982de2915258278db58aab6c78ca1aa6555a
Redweber2017 <gowtham17.rr@gmail.com>
2025-04-06 17:50:32 +0530

    first commit

---
+++ src/test/java/com/redweber/renderTest/RenderTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redweber.renderTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenderTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
